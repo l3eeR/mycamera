b56387441df1a7f403e4cdc995f4b6c6dfe792f4
l3eeR <beerb28@gmail.com>
2019-09-16 15:50:28 +0700

    Ber

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CameraScreen from './src/CameraScreen';
+
+export default class App extends React.Component {
+  return () {
+    this.return(
+      <CameraScreen/>
+    );
+  };
+};
+
